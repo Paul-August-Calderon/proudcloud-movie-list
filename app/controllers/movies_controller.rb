3d3e4fba5f7bfae5447c551aaaca300b70d692b9
Paul-August-Calderon <calderon.paul.august@gmail.com>
2025-05-07 18:37:02 +0800

    dynamically highlight row with highest rated movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,5 +2,6 @@ class MoviesController < ApplicationController
   def index
     # there might be a more performant way of doing this.
     @movies = Movie.find(Movie.ids.sample(3))
+    @highest_rating = @movies.max_by(&:vote_average)&.vote_average
   end
 end
