2108726ad201b95279dc69e356836faa810f8303
Paul-August-Calderon <calderon.paul.august@gmail.com>
2025-05-10 00:27:50 +0800

    add movies endpoints request spec

--- Gemfile
+++ Gemfile
@@ -68,7 +68,9 @@ group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
+  gem "rails-controller-testing"
 end
 
 gem "tailwindcss-ruby", "~> 4.1"
 gem "tailwindcss-rails", "~> 4.2"
+
--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -2,10 +2,39 @@ require 'rails_helper'
 
 RSpec.describe "Movies", type: :request do
   describe "GET /index" do
+    subject { get movies_path }
+
     it "returns http success" do
-      get "/movies/index"
+      subject
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders the expected template" do
+      subject
+      expect(response).to render_template(:index)
     end
   end
 
+  describe "post /search" do
+    subject { post search_movies_path(format: :turbo_stream) }
+
+    it "returns http success" do
+      subject
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the expected template" do
+      subject
+      expect(response).to render_template(:search)
+    end
+
+    context "when requesting the wrong format" do
+      subject { post search_movies_path }
+
+      it "returns http Not Acceptable" do
+        subject
+        expect(response).to have_http_status(:not_acceptable)
+      end
+    end
+  end
 end
