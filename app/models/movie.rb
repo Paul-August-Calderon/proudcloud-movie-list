f84651561d4e5bd4e1a98f2479dfb927b4cc3442
Paul-August-Calderon <calderon.paul.august@gmail.com>
2025-05-09 17:56:53 +0800

    add validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
+  validates :vote_average, :vote_count, numericality: { greater_than_or_equal_to: 0 }
+  validates :vote_average, :vote_count, :title, presence: :true
+
   scope :with_title_like, ->(query) {
     where("title LIKE ?", "%#{sanitize_sql_like(query)}%")
   }
