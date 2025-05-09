7be2fa8cadc5fb7f51d6bcf0af78270df3730f95
Paul-August-Calderon <calderon.paul.august@gmail.com>
2025-05-09 22:58:21 +0800

    refactor MoviesHelper method

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,7 +1,7 @@
 module MoviesHelper
 
-  def highlight_for_highest_rated_movie(current_vote, highest_vote)
+  def highlight_highest_rated(current_rating, highest_rating)
     # This method doesn't take into account tied movies. To handle tied movies, the movie id should be the params here.
-    current_vote == highest_vote ? "bg-green-300" : ""
+    current_rating == highest_rating ? "bg-green-300" : nil
   end
 end
